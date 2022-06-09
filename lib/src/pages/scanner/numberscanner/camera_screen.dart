a1a26d3cb189070bd858966f08110cfda9fdaf2c
Abdiel Carrasco <redskullcoder@gmail.com>
2022-06-08 17:01:14 -0700

    ISHUB21 - REFCTOR SCAN SCANNER PAGE

--- lib/src/pages/scanner/numberscanner/camera_screen.dart
+++ lib/src/pages/scanner/numberscanner/camera_screen.dart
@@ -1,19 +1,51 @@
-import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:ui';
 
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:http/http.dart' as http;
+import '../../../models/consultaBatch.dart';
 import 'detail_screen.dart';
 
-Future<void> main() async {
-  runApp(
-    MaterialApp(
-      title: 'Camera Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ExamplePage(),
-    ),
-  );
+var UsersID;
+var clientID;
+var ID;
+var batch_number;
+var batchID;
+var consoleGroup;
+var station;
+
+Future<List<ConsultaBatch>> fetchPhotos(http.Client client, String UsersID, String clientID, String ID ) async {
+  final response = await client
+      .get(Uri.parse('http://3.217.149.82/batchjobx/ws/ws_consultaBatch.php?UsersID=$UsersID&clientID=$clientID&bitacoraID=$ID'));
+
+  print(response.body);
+  // Use the compute function to run parsePhotos in a separate isolate.
+  return compute(parsePhotos, response.body);
 }
+
+// A function that converts a response body into a List<Photo>.
+List<ConsultaBatch> parsePhotos(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  print('print : $parsed');
+  return parsed.map<ConsultaBatch>((json) => ConsultaBatch.fromJson(json)).toList();
+}
+
+
+
+// Future<void> main() async {
+//   runApp(
+//     MaterialApp(
+//       title: 'Camera Example',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: ExamplePage(),
+//     ),
+//   );
+// }
 class ExamplePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => ExamplePageState();
@@ -71,25 +103,75 @@ class ExamplePageState extends State<ExamplePage> {
 
   @override
   Widget build(BuildContext context) {
+    final arguments = (ModalRoute.of(context)?.settings.arguments ?? <String, dynamic>{}) as Map;
+    print('Los elementos son : $arguments');
+    print(arguments['batch_number']);
+    print(arguments['ID']);
+    UsersID = arguments['UsersID'];
+    clientID = arguments['clientID'];
+    batch_number = arguments['batch_number'];
+    ID = arguments['ID'];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Camera example'),
-      ),
-      /*body: Stack(
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage("https://wallpaper.dog/large/10762816.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: ClipRRect( // make sure we apply clip it properly
+      child: BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 90, sigmaY: 90),
+      child: Column(
         children: [
-          _cameraPreview(),
-          Align(alignment: Alignment.bottomCenter, child: _button()),
+          Container(
+              alignment: Alignment.topCenter,
+              margin: EdgeInsets.only(top: 40),
+              child: Text(
+                'SCAN UTILITY',
+                textAlign:TextAlign.center,
+                style: TextStyle(fontSize: 28,
+                    fontFamily: 'Prompt-Italic',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              )
+          ),
+            Container(
+            margin: EdgeInsets.only(top: 0),
+            alignment: Alignment.topCenter,
+            child: Lottie.asset(
+              'assets/json/code3.json',
+              width: 120,
+
+            ),
+          ),
+          Container(
+            width: 500,
+              height: 300,
+              child: _cameraPreview()),
+          Container
+            (
+            margin: EdgeInsets.only(top:30),
+              child: _button()),
+           Center(
+              child: Container(
+                height: 0,
+                  margin: EdgeInsets.only(top: 0),
+                  child: Visibility(
+                    visible: false,
+                      child: _listAddress())
+              ),
+            ),
+         // ),
+
         ],
-      ),
-    );*/
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          _cameraPreview(),
-          const SizedBox(height: 48),
-          _button(),
-        ],
-      ),
+      )
+    // ))
+    //   ),
+      )
+    )
+    )
     );
   }
 
@@ -109,6 +191,82 @@ class ExamplePageState extends State<ExamplePage> {
     }
   }
 
+
+
+
+  //LIST ADRESS
+  Widget _listAddress() {
+    return FutureBuilder<List<ConsultaBatch>>(
+      future: fetchPhotos(http.Client(),UsersID,clientID,ID),
+      builder: (context, snapshot) {
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('An error has occurred!'),
+          );
+        } else if (snapshot.hasData) {
+          return Container(
+            child: Padding(
+                padding: const EdgeInsets.all(1),
+                child: ClipRRect(
+                  child: GridView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    gridDelegate:
+                    SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1,
+                      childAspectRatio: MediaQuery.of(context).size.width /
+                          (MediaQuery.of(context).size.height / 15),),
+                    itemBuilder: (BuildContext context, int index) {
+                      batchID =snapshot.data[index].ID;
+
+                      return Container(
+                        margin: new EdgeInsets.symmetric(horizontal: 25.0,vertical: 2.0),
+                        decoration: BoxDecoration(
+                          // color: const Color(0xff7c94b6),
+                          color: Colors.black,
+
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: InkWell(
+                            onTap: (){
+                              // Navigator.pushNamed(
+                              //   context,
+                              //   'scanner',
+                              //   arguments: {'batch_number':'${snapshot.data[index].batch_number}','ID':'${snapshot.data[index].ID}'},
+                              // );
+                            },
+
+                            child: Container(
+                              child: Text(
+                                ' ð Console Group :${snapshot.data[index].console_group} / ${snapshot.data[index].station}',
+                                textAlign:TextAlign.center,
+                                style: TextStyle(color: Colors.lightGreen,
+                                  fontSize: MediaQuery.of(context).size.width /
+                                      (MediaQuery.of(context).size.height / 33),
+                                ),
+
+                              ),
+                            ),
+
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: snapshot.data.length,
+                  ),
+                )),
+          );
+
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
   Widget _button() {
     print("############");
     return Ink(
@@ -116,36 +274,43 @@ class ExamplePageState extends State<ExamplePage> {
         color: Colors.lightBlue,
         shape: CircleBorder(),
       ),
-      child: IconButton(
-        icon: Icon(Icons.camera_alt),
-        color: Colors.white,
-          onPressed: () async {
-            // If the returned path is not null navigate
-            // to the DetailScreen
-            await _takePicture().then((String path) {
-              print("############"+path);
-              if (path != null) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetailScreen(
-                      imagePath: path,
-                    ),
-                  ),
-                );
-              } else {
-                print('Image path not found!');
-              }
-            });
-          },
+      child:ClipRRect(
+        borderRadius: BorderRadius.circular(120),
+        child: Container(
+          color: Colors.black,
+          child: IconButton(
+            iconSize: 60,
+            icon: Icon(Icons.camera_alt),
+            color: Colors.white,
+              onPressed: () async {
+                // If the returned path is not null navigate
+                // to the DetailScreen
+                await _takePicture().then((String path) {
+                  print("############"+path);
+                  if (path != null) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailScreen(
+                          imagePath: path,
+                        ),
+                      ),
+                    );
+                  } else {
+                    print('Image path not found!');
+                  }
+                });
+              },
 /////
+          ),
+        ),
       ),
     );
   }
   Widget _cameraPreview() {
     if (initialized) {
       return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32),
+        padding: const EdgeInsets.symmetric(horizontal: 82),
         child: _CroppedCameraPreview(
           cameraController: cameraController,
         ),
@@ -204,7 +369,7 @@ class _CroppedCameraPreview extends StatelessWidget {
           Container(
             decoration: ShapeDecoration(
               shape: CardScannerOverlayShape(
-                borderColor: Colors.red,
+                borderColor: Colors.yellow,
                 borderRadius: 12,
                 borderLength: 32,
                 borderWidth: 8,
@@ -218,7 +383,7 @@ class _CroppedCameraPreview extends StatelessWidget {
 }
 ////////////
 // クレカ標準の比
-const _CARD_ASPECT_RATIO = 1 / 6;
+const _CARD_ASPECT_RATIO = 1 / 9;
 // 横の枠線marginを決める時用のfactor
 // 横幅の5%のサイズのmarginをとる
 const _OFFSET_X_FACTOR = 0.05;
--- lib/src/pages/scanner/numberscanner/detail_screen.dart
+++ lib/src/pages/scanner/numberscanner/detail_screen.dart
@@ -1,12 +1,16 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
+import 'dart:convert' as convert;
+import 'package:easy_dialog/easy_dialog.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:http/http.dart' as http;
+import 'package:lottie/lottie.dart';
 import '../../../models/consultaBatch.dart';
+import '../scanner_page.dart';
+import 'camera_screen.dart';
 
 
 
@@ -66,10 +70,14 @@ class _DetailScreenState extends State<DetailScreen> {
     // Finding and storing the text String(s) and the TextElement(s)
     for (TextBlock block in text.blocks) {
       for (TextLine line in block.lines) {
-        print('text: ${line.text}');
+        print('textoEncontrado lineas : ${line.text}');
         if (regEx.hasMatch(line.text)) {
           emailStrings.add(line.text);
+
+          print('textoEmails emails : ${line.text}');
           for (TextElement element in line.elements) {
+            print('textoelements elements : ${line.text}');
+            _text(context,line.text);
             _elements.add(element);
            // pref.setStringList('scan', _elements);
 
@@ -138,7 +146,7 @@ class _DetailScreenState extends State<DetailScreen> {
                     Padding(
                       padding: const EdgeInsets.only(bottom: 8.0),
                       child: Text(
-                        "Identified emails",
+                        "LOG ALGORITM DETECTED FROM IMAGE",
                         style: TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
@@ -154,10 +162,10 @@ class _DetailScreenState extends State<DetailScreen> {
                           physics: BouncingScrollPhysics(),
                           itemCount: _listEmailStrings.length,
                           itemBuilder: (context, index) =>
-                          _text(_listEmailStrings[index]),
-                              // Text(_listEmailStrings[index]
-                              //
-                              // ),
+                          // _text(context,_listEmailStrings[index],index),
+                              Text(_listEmailStrings[index]
+
+                              ),
                         )
                             : Container(),
                       ),
@@ -178,17 +186,115 @@ class _DetailScreenState extends State<DetailScreen> {
     );
   }
 
-   Widget _text(String _lista){
+   Widget _text(BuildContext context, String _lista){
+     print('WsAbdiel valor númerico de la lista :{  } valor contenido de la lista : { $_lista }');
      if(_lista == "4000888164"){
-       return Text("40000888164");
+     //  print('WsAbdiel valido $station');
+       getScanner(context,"4000888164","2","5");
+       return Text("400888164");
+
      }else{
-       return Text("NULL");
+       //return Text("NULL");
      }
 
    }
 }
 
+Future<List<ConsultaBatch>> getScanner(context,String scanner, String bitacora, String station) async {
+  print('wsbarcode $scanner ande the bitacora $bitacora');
+  print('WsAbdiel 1 $station');
+  var url = 'http://3.217.149.82/batchjobx/ws/ws_valida_scanner.php?scanner=$scanner&bitacora=$bitacora';
+  print(url);
+  // Await the http get response, then decode the json-formatted response.
+  var response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    var scanws = jsonResponse['SCAN'];
 
+    print('WsAbdiel ### $scanws');
+    if(scanws != 'SCAN ERROR'){
+     // AudioCache player = AudioCache();
+     // player.play('sounds/beep.mp3');
+      print('WsAbdiel 2 $station');
+    //  _dialogSucces(scanws);
+      EasyDialog(
+          closeButton: true,
+          width: 280,
+          height: 500,
+          contentPadding:
+          EdgeInsets.only(top: 1.0), // Needed for the button design
+          contentList: [
+            Container(
+              child: Lottie.asset(
+                'assets/json/success2.json',
+                width: 200,
+                height: 200,
+              ),
+            ),
+            Container(
+              child: Text(
+                "Success!! we found the station",
+                style: TextStyle(fontWeight: FontWeight.bold,color: Colors.teal),
+                textScaleFactor: 1.2,
+              ),
+            ),
+            Container(
+              child: Text(
+                "$station",
+                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue[900]),
+                textScaleFactor: 2.2,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                "$station",
+                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue[900]),
+                textScaleFactor: 6.2,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.greenAccent,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10.0),
+                      bottomRight: Radius.circular(10.0))),
+              child: FlatButton(
+                onPressed: () {
+                 // Navigator.of(context).pop();
+                 //  Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
+                 //      ExamplePage()), (Route<dynamic> route) => false);
+
+                  Navigator.pushAndRemoveUntil<void>(
+                    context,
+                    MaterialPageRoute<void>(builder: (BuildContext context) => ExamplePage()),
+                    ModalRoute.withName('scanner',),
+                  );
+                },
+                child: Text(
+                  "Ok",
+                  style: TextStyle(color: Colors.black87),
+                  textScaleFactor: 1.3,
+                ),
+              ),
+            ),
+          ]).show(context);
+
+    }else{
+      // _dialogFail();
+      // AudioCache player = AudioCache();
+      // player.play('sounds/fail.mp3');
+
+    }
+
+  } else {
+   // _dialogFailNet();
+  }
+
+
+
+}
 
 // Helps in painting the bounding boxes around the recognized
 // email addresses in the picture
